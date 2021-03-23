e1fa07437bcf9abcbd3f8b060ad74711070a7c52
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-03-23 21:58:17 +0300

    Update test.c

--- test.c
+++ test.c
@@ -7,7 +7,11 @@
 int main() {
   printf("\n\n %f \n\n", fabs(CostOfTheDistillation(10) - 15.55));
   assert(fabs(CostOfTheDistillation(10) - 15.55) < 0.01);
+  
+  printf("\n\n %f \n\n", fabs(CostOfTheCustoms(2, 3300) - 3.73));
   assert(fabs(CostOfTheCustoms(2, 3300) - 3.73) < 0.01);
+  
+  printf("\n\n %f \n\n", fabs(TotalCostInByn(10000, 10, 2, 3300) - 31119.28));
   assert(fabs(TotalCostInByn(10000, 10, 2, 3300) - 31119.28) < 0.01);
   return 0;
 }
