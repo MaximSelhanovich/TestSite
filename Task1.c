929c8813eabc1631906452cfd020102c69447376
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-03-23 21:27:13 +0300

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,51 @@
+#include "functions.c"
+
+int main()
+{
+    unsigned int number, year, cost, value,distance;
+    while(1)
+    {
+        Menu();
+        printf("Enter the number of the function: ");
+        switch(EnterTheNumber(number))
+        {
+            case 1: 
+                clrscr();
+                printf("The cost of the car (€): ");
+                scanf("%u", &cost);
+                clrscr();
+                break;
+            case 2: 
+                clrscr();
+                printf("The year of the car: ");
+                scanf("%u", &year);
+                clrscr();
+                break;
+            case 3:
+                clrscr();
+                printf("The engine value of the car (cc):  ");
+                scanf("%u", &value);
+                clrscr();
+                break;
+            case 4:
+                clrscr();
+                printf("The run distance of the car (km): ");
+                scanf("%u", &distance);
+                clrscr();
+                break;
+            case 5:
+            clrscr();
+                PresentData(distance, year, value, cost);
+                break;
+            case 6:
+                clrscr();
+                Information();
+                break;
+            case 7:
+                clrscr();
+                exit(0);
+                break;
+        }
+    }
+}
+
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include "functions.h"
+
+#undef main
+int main() {
+  assert(fabs(CostOfTheDistillation(10) - 15.55) < 0.01);
+  assert(fabs(CostOfTheCustoms(2, 3300) - 3.73) < 0.01);
+  assert(fabs(TotalCostInByn(10000, 10, 2, 3300) - 31119.28) < 0.01);
+  return 0;
+}
