d3957608d5a57045426de0670e87247f0837f880
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-03-30 21:14:46 +0300

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -3,32 +3,23 @@
 #include <time.h>
 #include <malloc.h>
 
-int surround(int x, int y, int verticalFlaf, int type, int** field)
-{   
+int surround(int x, int y, int verticalFlaf, int type, int** field) {   
     int  i, j;
-    if (verticalFlaf == 1)
-    {
-        for (i = x - 1; i <= x + 1; i++)
-        {
+    if (verticalFlaf == 1) {
+        for (i = x - 1; i <= x + 1; i++) {
             if (i >= 0 && i < 10) {
-                for (j = y - 1; j <= y + type; j++)
-                {
-                    if (j >= 0 && j < 10 && field[i][j] == 0)
-                    {
+                for (j = y - 1; j <= y + type; j++) {
+                    if (j >= 0 && j < 10 && field[i][j] == 0) {
                         field[i][j] = 8;
                     }
                 }
             }
         }
     }
-    else
-    {
-        for (i = x - 1; i <= x + type; i++)
-        {
-            if (i >= 0 && i < 10) 
-            {
-                for (j = y - 1; j <= y + 1; j++)
-                {
+    else {
+        for (i = x - 1; i <= x + type; i++) {
+            if (i >= 0 && i < 10) {
+                for (j = y - 1; j <= y + 1; j++) {
                     if (j >= 0 && j < 10 && field[i][j] == 0)
                         field[i][j] = 8;
                 }
@@ -37,38 +28,29 @@ int surround(int x, int y, int verticalFlaf, int type, int** field)
     }
     return 0;
 }
-void fill(int** field)
-{
+
+void fill(int** field) {
     int i, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             field[i][j] = 0;
         }
     }
 }
 
-void clear(int** field)
-{
+void clear(int** field) {
     int i, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             if(field[i][j] == 8) field[i][j] = 0;
         }
     }
-
 }
 
-void printField(int** field)
-{
+void printField(int** field) {
     int i, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             printf("%d  ", field[i][j]);
         }
         printf("\n");
@@ -113,33 +95,25 @@ int verticalCheck (int** field, int y, int x, int size) {
     return 1;
 }
 
-void place(int** field)
-{
+void place(int** field) {
     int type = 4, fillIndex = 0, y, x, amount = 1, verticalFlaf;
 
-    for (type = 4; type > 0; --type)
-    {
+    for (type = 4; type > 0; --type) {
         amount = 4 - type + 1;
-        for (amount = 4 - type + 1; amount > 0; --amount)
-        {
+        for (amount = 4 - type + 1; amount > 0; --amount) {
             fillIndex = 0;
-            while (fillIndex == 0) 
-            {
-                do
-                {
+            while (fillIndex == 0) {
+                do {
                     y = rand() % 10;
                     x = rand() % 10;
                 } while (field[y][x] != 0);
                 
-                if (horizontalCheck(field, y, x, type) == 1)
-                {
+                if (horizontalCheck(field, y, x, type) == 1) {
                     fillIndex = horizontalFill(field, y, x, type);
                     verticalFlaf = 1;
                 }
-                else
-                {
-                    if (verticalCheck(field, y, x, type) == 1)
-                    {
+                else {
+                    if (verticalCheck(field, y, x, type) == 1) {
                         fillIndex = verticalFill(field, y, x, type);
                         verticalFlaf = 0;
                     }
@@ -150,27 +124,22 @@ void place(int** field)
     }
 }
 
-int checkShips(int** field)
-{
+int checkShips(int** field) {
     int i, j, countSh = 0;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (field[i][j] > 0 && field[i][j] < 5)
-            {
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            if (field[i][j] > 0 && field[i][j] < 5) {
                 countSh += field[i][j];
             }
         }
     }
     return countSh;
 }
-int main(void)
-{
+int main(void) {
     int n = 10, m = 10, i, amount;
     int** field = (int**)malloc(n * sizeof(int*));
-    for (i = 0; i < n; i++)
-    {
+    
+    for (i = 0; i < n; i++) {
         field[i] = (int*)malloc(m * sizeof(int));
     }
 
@@ -185,8 +154,7 @@ int main(void)
     amount = checkShips(field);
     printf("am = %d", amount);
 
-    for (i = 0; i < n; i++) 
-    {
+    for (i = 0; i < n; i++) {
         free(field[i]);
     }
     free(field);
