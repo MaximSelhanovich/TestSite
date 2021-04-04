24265551ed58213213926f5a61348789b08a15ad
ClGelver <75281453+MaximSelhanovich@users.noreply.github.com>
2021-04-04 17:24:07 +0300

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -1,161 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <malloc.h>
 
-int surround(int x, int y, int verticalFlaf, int type, int** field) {
-    int  i, j;
-    if (verticalFlaf == 1) {
-        for (i = x - 1; i <= x + 1; i++) {
-            if (i >= 0 && i < 10) {
-                for (j = y - 1; j <= y + type; j++) {
-                    if (j >= 0 && j < 10 && field[i][j] == 0) {
-                        field[i][j] = 8;
-                    }
-                }
-            }
-        }
-    } else {
-        for (i = x - 1; i <= x + type; i++) {
-            if (i >= 0 && i < 10) {
-                for (j = y - 1; j <= y + 1; j++) {
-                    if (j >= 0 && j < 10 && field[i][j] == 0)
-                        field[i][j] = 8;
-                }
-            }
-        }
+int checkNULL (void *ptr) {
+    if(ptr == NULL) {
+        printf("Allocation error!\n");
+        return 1;
     }
     return 0;
 }
 
-void fill(int** field) {
-    int i, j;
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            field[i][j] = 0;
+/*Does not include '\n' and '\0' when counting*/
+unsigned int lineLength(char *line) {
+    unsigned int counter = 0;
+    while(line[counter] && line[counter] != '\n') ++counter;
+    return counter;
+}
+
+void *clearTwoTwoDimensionalArray(void **arrayToClean,
+                                  unsigned int rowsNumber) {
+    unsigned int i = 0;
+    
+    if(!arrayToClean) {
+        printf("This array is already clean.\n");
+        return NULL;
+    }
+    
+    for(i = 0; i < rowsNumber; ++i) free(arrayToClean[i]);
+    free(arrayToClean);
+
+    printf("Memory cleaned\n");
+    return NULL;
+}
+
+char ** getTwoDimensionalArray(unsigned int linesNumber) {
+    char **text = NULL;
+    unsigned int i, success = 1;
+
+    text = (char **)malloc(linesNumber * sizeof(char *));
+    if(checkNULL(text)) return clearTwoTwoDimensionalArray((void **)text, 0);
+
+    for(i = 0; i < linesNumber; ++i) {
+        text[i] = (char *)malloc(255 * sizeof(char *));
+        if(checkNULL(text[i])) {
+            success = 0; 
+            break;
         }
     }
+    if (!success) return clearTwoTwoDimensionalArray((void **)text, i);
+
+    return text;
 }
 
-void clear(int** field) {
-    int i, j;
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            if (field[i][j] == 8) field[i][j] = 0;
-        }
-    }
-}
+void getText(char **text, int *linesNumber) {
+    unsigned int i = 0;
+    char *tempLine = NULL;
+    char **tempText = NULL;
 
-void printField(int** field) {
-    int i, j;
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            printf("%d  ", field[i][j]);
-        }
-        printf("\n");
-    }
-}
+    while (getchar() != '\n') {
+        if (i >= *linesNumber) {
+            tempText =  (char **)realloc(text, *linesNumber + 3);
 
-int horizontalCheck(int** field, int y, int x, int size) {
-    int i = 0;
-    if (x + size - 1 > 9) return 0;
-
-    for (i = 0; i < size; ++i) {
-        if (field[y][x + i] != 0) return 0;
-    }
-    return 1;
-}
-
-int horizontalFill(int** field, int y, int x, int size) {
-    int i = 0;
-
-    for (i = 0; i < size; ++i) {
-        field[y][x + i] = size;
-    }
-    return 1;
-}
-
-int verticalFill(int** field, int y, int x, int size) {
-    int i = 0;
-
-    for (i = 0; i < size; ++i) {
-        field[y + i][x] = size;
-    }
-    return 1;
-}
-
-int verticalCheck(int** field, int y, int x, int size) {
-    int i = 0;
-    if (y + size - 1 > 9) return 0;
-
-    for (i = 0; i < size; ++i) {
-        if (field[y + i][x] != 0) return 0;
-    }
-    return 1;
-}
-
-void place(int** field) {
-    int type = 4, fillIndex = 0, y, x, amount = 1, verticalFlaf;
-
-    for (type = 4; type > 0; --type) {
-        amount = 4 - type + 1;
-        for (amount = 4 - type + 1; amount > 0; --amount) {
-            fillIndex = 0;
-            while (fillIndex == 0) {
-                do {
-                    y = rand() % 10;
-                    x = rand() % 10;
-                } while (field[y][x] != 0);
-
-                if (horizontalCheck(field, y, x, type) == 1) {
-                    fillIndex = horizontalFill(field, y, x, type);
-                    verticalFlaf = 1;
-                } else {
-                    if (verticalCheck(field, y, x, type) == 1) {
-                        fillIndex = verticalFill(field, y, x, type);
-                        verticalFlaf = 0;
-                    }
-                }
+            if(checkNULL(tempText)) {
+                clearTwoTwoDimensionalArray((void **)text, *linesNumber);
+                return;
             }
-            surround(y, x, verticalFlaf, type , field);
+            text = tempText;
+            *linesNumber += 3;
         }
+
+        scanf(" %[^\n]254s", text[i]);
+        getchar();
+        tempLine = (char *)realloc(text[i], lineLength(text[i] + 1));
+        if(checkNULL(tempLine)) {
+            clearTwoTwoDimensionalArray((void **)text, *linesNumber);
+            return;
+        }
+        text[i] = tempLine;
+        ++i;
     }
 }
 
-int checkShips(int** field) {
-    int i, j, countSh = 0;
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            if (field[i][j] > 0 && field[i][j] < 5) {
-                countSh += field[i][j];
-            }
+int main() {
+    int linesNumber = 5; 
+    char **text = NULL;
+
+    text = getTwoDimensionalArray(linesNumber);
+    if(!text) return 1;
+    getText(text, &linesNumber);
+    printf("May be yes may be no");
+    /*text = (char **)malloc(rowsNumber * sizeof(char *));
+    if(!text) {
+        printf("Allocation error!\n");
+        free(text);
+        text = NULL;
+        return 1;
+    }
+
+    for(; i < rowsNumber; ++i) {
+        text[i] = (char *)malloc(255 * sizeof(char));
+        if(!text[i]) {
+            printf("Allocation error!");
+            success = 0;
+            break;
         }
     }
-    return countSh;
-}
-int main(void) {
-    int n = 10, m = 10, i, amount;
-    int** field = (int**)malloc(n * sizeof(int*));
 
-    for (i = 0; i < n; i++) {
-        field[i] = (int*)malloc(m * sizeof(int));
-    }
+    if (success == 0) {
+        for (; i >= 0; i--) free(text[i]);
+        free(text);
+        text = NULL;
+        return 1;
+    }*/
 
-    srand(time(NULL));
 
-    printf("Autoplacement of ships\n");
-    fill(field);
-    place(field);
-    clear(field);
-    printf("final version:\n");
-    printField(field);
-    amount = checkShips(field);
-    printf("am = %d", amount);
-
-    for (i = 0; i < n; i++) {
-        free(field[i]);
-    }
-    free(field);
-
+    text = clearTwoTwoDimensionalArray((void **)text, linesNumber);
     return 0;
 }
